fe808e1ed04b7f41ebc30b750c64f26a7b901171
Michael <mcopley08@comcast.net>
2015-11-07 10:50:39 -0500

    fixing route because two were the same... :p

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
     var chefware = require('./controllers/chefware');
     app.get('/api/:username', chefware.joinServer);
-    app.get('/api/:user_id', chefware.getUserProgress);
+    app.get('/api/status/:user_id', chefware.getUserProgress);
     app.post('/api/timer/:user_id', chefware.updateTimer);
 }

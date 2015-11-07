33212cde4bbca0f9ba8f7695e1e65fc04eabe5bd
Michael <mcopley08@comcast.net>
2015-11-07 10:54:06 -0500

    fixing the status update endpoint

--- controllers/chefware.js
+++ controllers/chefware.js
@@ -22,7 +22,12 @@ exports.getUserProgress = function(event, res) {
     // this is a HACKY way to parse the url parameters
     var url_params = event.url.split("/");
     console.log(url_params);
-    var user_id_in = url_params[2];
+    var user_id_in = url_params[3];
+
+    // error checking - should never exeute
+    if (user_id_in === "") {
+        res.send(400, 'Bad Request: You need to specify a user id.');
+    }
 
     UserProgress.find({ user_id: user_id_in }, { "keywords": 0 }, function(err, userProgress) {
     	if (err) {
